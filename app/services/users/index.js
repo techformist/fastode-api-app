63c924a21995a177d67f3fcc769b89d88e0c1d51
Prashanth Krishnamurthy <prashanth1krishnamurthy@gmail.com>
2020-03-19 09:51:00 +0530

    initial version - kitchen sink in app

--- app/services/users/index.js
+++ app/services/users/index.js
@@ -6,6 +6,12 @@ module.exports = async function(fastify, opts) {
     // reply.notFound()
     //throw fastify.httpErrors.notFound()
 
+    //  const urlData = req.urlData()
+    // req.log.info(urlData.path) // '/foo'
+    // req.log.info(urlData.query) // 'a=b&c=d'
+    // req.log.info(urlData.host) // '127.0.0.1'
+    // GET: 'http://127.0.0.1:8080/foo?a=b&c=d
+
     return "/users";
   });
 };
